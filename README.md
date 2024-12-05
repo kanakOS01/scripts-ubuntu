87b6054331ad34d9605d2667b3f4034b874ae79c
Kanak Tanwar <kanaktanwarpro@gmail.com>
2024-12-05 11:41:23 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Basic Installation script for Ubuntu 24.04
+
+	Should not be run as a bash script.
+	Open the file and run the commands individually according to the packages you want to install.
---
+++ initial-script.sh
@@ -0,0 +1,84 @@
+sudo apt update
+
+
+## Extensions Manager
+sudo apt install gnome-shell-extension-manager
+sudo apt-get install ubuntu-tweak
+
+
+## Git
+sudo apt install git-all
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+gh auth login
+git config --global user.email "kanaktanwarpro@gmail.com"
+git config --global user.name "Kanak Tanwar"
+
+
+## Zsh
+sudo apt install zsh -y
+chsh -s /usr/bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt-get install fonts-powerline
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sudo apt-get install dconf-cli
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+
+## Docker
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+docker login
+sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
+su - ${USER}
+groups
+
+
+## Docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.31.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose  # check version before installing
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+
+## Brave Browser
+sudo apt install curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
+
+## Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm -rf google-chrome-stable_current_amd64.deb
+
+
+## VS Code
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
+
+## Postman
+snap install postman
+
+
+## Postgress
+sudo apt install postgresql
+curl -o pgadmin-key.gpg https://www.pgadmin.org/static/packages_pgadmin_org.pub
+sudo mv pgadmin-key.gpg /etc/apt/trusted.gpg.d/
+
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/focal/ pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list'
+sudo apt update
+sudo apt install pgadmin4
