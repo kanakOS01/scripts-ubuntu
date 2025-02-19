5d1cff7168be650fb909b886aae2b8b2422a3365
Kanak Tanwar <kanaktanwarpro@gmail.com>
2025-02-19 10:19:00 +0530

    Update initial-script.sh

--- initial-script.sh
+++ initial-script.sh
@@ -97,3 +97,15 @@ pipx ensurepath
 
 pipx install poetry
 pipx inject poetry poetry-plugin-shell
+
+
+## Pyenv
+curl -fsSL https://pyenv.run | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init - zsh)"' >> ~/.zshrc
+exec $SHELL
+
+sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl git \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
