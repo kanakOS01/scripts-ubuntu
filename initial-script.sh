62b253637f08572115c60c45707efea3bff17bb2
Kanak Tanwar <kanaktanwarpro@gmail.com>
2025-02-21 13:30:37 +0530

    Update initial-script.sh

--- initial-script.sh
+++ initial-script.sh
@@ -36,11 +36,11 @@ sudo apt update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-docker login
 sudo apt install docker-ce -y
 sudo usermod -aG docker ${USER}
 su - ${USER}
 groups
+docker login
 
 
 ## Docker compose
