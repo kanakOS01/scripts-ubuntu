aaf81533aa6f91a4fe08623c8ceefb0f34217585
Kanak Tanwar <kanaktanwarpro@gmail.com>
2025-02-21 13:33:55 +0530

    Update initial-script.sh

--- initial-script.sh
+++ initial-script.sh
@@ -36,7 +36,7 @@ sudo apt update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt install docker-ce -y
+sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker ${USER}
 su - ${USER}
 groups
